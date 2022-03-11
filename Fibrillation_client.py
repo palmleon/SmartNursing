ca50ff6d607b9707f990c76fa51891cccd8021cc
Lorenzo Centamore <lorenzocentamore@MacBook-Pro.local>
2022-03-11 18:48:43 +0100

    Creazione dei client (no richieste REST)

--- Fibrillation_client.py
+++ Fibrillation_client.py
@@ -1,4 +1,4 @@
-from Patient_monitor_Analyzer import *
+from Fibrillation_Analyzer import *
 from MyMQTT import MyMQTT
 import time
 
@@ -8,14 +8,13 @@ class Patient_Monitor_client():
     self.__clientID="clientID"
     self.__broker="broker"
     self.__port=1883
-    self.__topic_sub_T="group01/IoTProject/Patient/Temperature/+"
-    self.__topic_sub_P="group01/IoTProject/Patient/Pulsation/+"
+    self.__topic_sub="group01/IoTProject/Patient/Pulsation/+"
     self.__base_topic_pub="group01/IoTproject/PatientAlarm/"
     # Generazione template allert
     self.__allert={"ID_PZ":"","allert":"","time":0}
 
     # Creating analyzer
-    self.analyzer=Patient_Monitor()
+    self.analyzer=Fibrillation_Monitor()
 
     # Creating client
     self.client = MyMQTT(self.__clientID, self.__broker, self.__port, self)
@@ -25,8 +24,7 @@ class Patient_Monitor_client():
     time.sleep(2)
 
     # Subscribing
-    self.client.mySubscribe(self.__topic_sub_T)
-    self.client.mySubscribe(self.__topic_sub_P)
+    self.client.mySubscribe(self.__topic_sub)
 
   def notify(self,topic,msg): # Metodo che analizza i dati arrivati utilizzando i metodi dell'analyzer e, in caso, pubblica i warning
 
@@ -51,26 +49,13 @@ class Patient_Monitor_client():
       
       if ev["n"]=="pulse rate":
         pulse=ev["v"]
-      
-      if ev["n"]=="temperature":
-        temp=ev["v"]
-    
-    if temp>-1:
-      r=self.analyzer.Temperature(ID_P,temp,battery)
-      if len(r)>1:
-        to_pub=self.__allert
-        to_pub["ID_PZ"]=ID_P
-        to_pub["allert"]=r
-        to_pub["time"]=time.time()
-        # Publish allert
-        self.client.myPublish(self.__base_topic_pub+ID_P,to_pub)
     
     if len(Pi)>1:
-      r=self.analyzer.Pulse(ID_P,Pi,pulse,sat,battery)
+      r=self.analyzer.fibrillation(ID_P,Pi,pulse,sat,battery)
       if len(r)>1:
         to_pub=self.__allert
         to_pub["ID_PZ"]=ID_P
         to_pub["allert"]=r
         to_pub["time"]=time.time()
         # Publish allert
-        self.client.myPublish(self.__base_topic_pub+ID_P,to_pub)
+        self.client.myPublish(self.__base_topic_pub+"/"+ID_P,to_pub)
--- Patient_monitor_Analyzer.py
+++ Patient_monitor_Analyzer.py
@@ -1,34 +1,6 @@
 import numpy as np
 
 class Patient_Monitor():
-    '''
-    Suppongo che la classe di elaborazione generi solo i messaggi da mandare a telegram (tramite classe client).
-    I dati da mandare a Thingspeak, dato che non devono essere elaborati prima, li manderà direttamente
-    il message broker a Thingspeak in modo da potersi sottoscrivere a specifico ID_paziente e quindi
-    specifico ID_pulse e ID_Termo, per quanto riguarda sia dati registrati che batteria'''   
-
-
-    '''
-    ATTENZIONE si dovrebbe pensare ad un modo (eventualmente nel client) per cui,
-    dopo aver restituito un messaggio di emergenza, si silenzi per un po' di tempo. Altrimenti, se si riceve
-    per lo stesso paziente una segnalazione di warning ogni 10s, l'infermiere impazzisce
-    (considerando più pazienti presenti).
-
-    Si può anche pensare che l'infermiere, una volta ricevuto il messaggio non riceve più altri warning per quel paziente
-    e che ritorna a riceverli se si verifica una di due condizioni: preme un tasto di conferma su telegram
-    (scrivendo tipo /ok [paziente ID] e si genera quindi una richiesta GET verso il servizio client
-    (nell'url metti l'ID PZ) che riattiva l'analisi) o passa più di tot tempo''' # Deve farlo Telegram
-
-
-    '''
-    I Return sono scritti in due modi possibili:
-    -return dell'intera stringa del messaggio da far vedere all'infermiere
-    -return con codice allarme (che corrisponde ad un messaggio specifico) e valori da stampare (se serve, anche ID)
-    Nel secondo caso il bot telegram, una volta ricevuto il messaggio, associa ad un codice allarme
-    un template di stringa da riempire con i campi necessari.
-    In questo modo, si risparmia sui bit da inviare sulla rete (minor latenza, minor probabilità di errore)''' # Da verificare successivamente
-
-
     def __init__(self):
         pass
 
@@ -37,18 +9,22 @@ class Patient_Monitor():
 
         # battery è la sola tensione di batteria
         if battery<2.8: # Per informare che il termometro si sta scaricando (es. suppongo sia scarico a 2.5V)
-            return f"ATTENZIONE, il paziente {ID_P} ha il termometro quasi scarico"
+            r=f"ATTENZIONE, il paziente {ID_P} ha il termometro quasi scarico"
+            return r
             #return [1, self.__ID_P]
 
         # incoming è solo il valore di temperatura
         if incoming<=35:# Il sensore è mal posizionato
-            return f"ATTENZIONE, il paziente {ID_P} ha il termometro mal posizionato"
+            r=f"ATTENZIONE, il paziente {ID_P} ha il termometro mal posizionato"
+            return r
             #return [2, ID_P]
         if incoming<36:
-            return f"ATTENZIONE, il paziente {ID_P} ha una temperatura bassa, pari a {incoming}"
+            r=f"ATTENZIONE, il paziente {ID_P} ha una temperatura bassa, pari a {incoming}"
+            return r
             #return [3, ID_P, incoming]
         if incoming>=37:
-            return f"ATTENZIONE, il paziente {ID_P} ha febbre, con temperatura pari a {incoming}"
+            r=f"ATTENZIONE, il paziente {ID_P} ha febbre, con temperatura pari a {incoming}"
+            return r
             #return [4, ID_P, incoming]
     
     def Pulse(self,ID_P,PI,pulse,sat,battery):
@@ -57,12 +33,14 @@ class Patient_Monitor():
         
         # battery è la sola tensione di batteria
         if battery<2.8:# Per informare che il pulsossimetro si sta scaricando (es. suppongo sia scarico a 2.5V)
-            return f"ATTENZIONE, il paziente {ID_P} ha il pulsossimetro quasi scarico"
+            r=f"ATTENZIONE, il paziente {ID_P} ha il pulsossimetro quasi scarico"
+            return r
             #return [5, ID_P]
 
         if max(PI)<4: # Cercando su internet, la soglia per una lettura attendibile è al 4%
             # Se nessuna lettura attendibile, inutile proseguire
-            return f"ATTENZIONE, il paziente {ID_P} ha il pulsossimetro mal posizionato o l'ha rimosso"
+            r=f"ATTENZIONE, il paziente {ID_P} ha il pulsossimetro mal posizionato o l'ha rimosso"
+            return r
             #return [6, ID_P]
 
         to_remove=[]
