aaa0b7e7745c09887dfbd3c1abb40c03ce49c720
alessda <99904423+alessda@users.noreply.github.com>
2022-04-02 20:33:52 +0200

    Update thingSpeak_matlab.m

--- thingSpeak_matlab/thingSpeak_matlab.m
+++ thingSpeak_matlab/thingSpeak_matlab.m
@@ -1,47 +1,37 @@
-clear all
-close all
-clc
-%% Library
-import matlab.net.*
-import matlab.net.http.*
-%% Get request
-r = RequestMessage;
-uri = URI('https://api.thingspeak.com/channels.json?api_key=YTN43QRA4IWIEBIN');
-resp = send(r,uri);
-%% Retrive data
-channel = resp.Body.Data(1);
-data = thingSpeakRead(channel.id,'Fields',[1,2,3,4],NumPoints=8000,OutputFormat='matrix',ReadKey=channel.api_keys(2).api_key);
+% Template MATLAB code for visualizing data from a channel as a 2D line
+% plot using PLOT function.
+
+% Prior to running this MATLAB code template, assign the channel variables.
+% Set 'readChannelID' to the channel ID of the channel to read from. 
+% Also, assign the read field ID to 'fieldID1'. 
+
+% TODO - Replace the [] with channel ID to read data from:
+readChannelID = 1693205;
+% TODO - Replace the [] with the Field ID to read data from:
+fieldID1 = 1;
+
+% Channel Read API Key 
+% If your channel is private, then enter the read API
+% Key between the '' below: 
+readAPIKey = '6BOT4EPEGVUBWPWC';
+
+%% Read Data %%
+data = thingSpeakRead(1693205,'Fields',[2,3,4,6],NumPoints=8000,OutputFormat='matrix',ReadKey='6BOT4EPEGVUBWPWC')
 % remove NaN data
 for col=1:width(data)
     i = 1;
     for row=1:height(data)
         if ~isnan(data(row,col))
-            n_data(i,col) = data(row,col);
+            n_data(i,col) = data(row,col)
             i = i+1;
         end
     end
 end
-% temperature average (temperature is always in the first column)
-temp_sum = 0;
-count = 0;
-for row=1:height(n_data)
-    if n_data(row) >= 35
-        temp_sum = temp_sum + n_data(row);
-        count = count + 1;
-    end
-end
-temp_avg = temp_sum/count
-% avg saturation and pulse rate
-sum_sat = 0;
-sum_pulse = 0;
-count = 0;
-for row=1:height(n_data)
-    if n_data(row,4) >= 4
-        sum_sat = sum_sat + n_data(row,2);
-        sum_pulse = sum_pulse + n_data(row,3);
-        count = count + 1;
-    end
-end
-avg_pulse = sum_pulse/count
-avg_sat = sum_sat/count
+
+%% Visualize Data %%
+bar(10,2)
+hold on
+grid on
+bar(1)
+title("title")
 
