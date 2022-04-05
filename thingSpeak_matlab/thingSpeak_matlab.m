b16f5af7fbf0b26fae5a631b26fbf80f42998b20
alessda <alexd9825@gmail.com>
2022-04-05 20:43:59 +0200

    code plotting correctly

--- thingSpeak_matlab/thingSpeak_matlab.m
+++ thingSpeak_matlab/thingSpeak_matlab.m
@@ -1,3 +1,6 @@
+clear all
+close all
+clc
 %% Library
 import matlab.net.*
 import matlab.net.http.*
@@ -16,41 +19,72 @@ for i=1:len
     temperature = data(:,4).temperature;
     saturation = data(:,2).saturation;
     pulseRate = data(:,3).pulseRate;
-return %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-end
 % Remove NAN elements
     k=1;
-    sum_index = 0;
-    h = 1;
     for j=1:length(perfusion_index)
         if ~isnan(perfusion_index(j))
-            if perfusion_index(j)>4
                 n_perfusion_index(k) = perfusion_index(j);
-                if ~isnan(saturation(j))
-                sum_index = sum_index + n_perfusion_index(k);
                 k = k+1;
-            end
         end
     end
-    avg_perfusion_index = sum/(k-1);
     k=1;
-    sum = 0;
     for j=1:length(temperature)
         if ~isnan(temperature(j))
-            if temperature(j)>35
-                n_temperature(k) = temperature(j);
-                sum = sum + n_temperature(k);
-                k = k+1;
-            end
+            n_temperature(k) = temperature(j);
+            k = k+1;
         end
     end
-    avg_temp(i) = sum/(k-1); 
+    k = 1;
+    for j=1:length(saturation)
+        if ~isnan(saturation(j))
+            n_saturation(k) = saturation(j);
+            k = k +1;
+        end
+    end
+    k = 1;
+    for j=1:length(pulseRate)
+        if ~isnan(pulseRate(j))
+            n_pulseRate(k) = pulseRate(j);
+            k = k +1;
+        end
+    end
+    % check parameters and average
+    sum_temp = 0;
+    k = 0;
+    for j=1:length(n_temperature)
+        if n_temperature(j) >= 35
+            sum_temp = sum_temp + n_temperature(j);
+            k = k + 1;
+        end
+    end
+    avg_temp(i) = sum_temp/k;
+    sum_perfusion = 0;
+    sum_saturation = 0;
+    sum_pulse = 0;
+    k = 0;
+    for j=1:length(n_perfusion_index)
+       if n_perfusion_index(j) > 4
+            sum_perfusion = sum_perfusion + n_perfusion_index(j);
+            sum_saturation = sum_saturation + n_saturation(j);
+            sum_pulse = sum_pulse + n_pulseRate(j);
+            k = k + 1;
+       end
+    end
+    avg_pulse(i) = sum_pulse/k;
+    avg_saturation(i) = sum_saturation/k;
+    avg_perfusion(i) = sum_perfusion/k;
+    % Visualize Data
+    bar(1,avg_perfusion(i))
+    hold on
+    grid on
+    bar(2,avg_saturation(i))
+    hold on 
+    grid on
+    bar(3,avg_pulse(i))
+    hold on
+    grid on
+    bar(4,avg_temp(i))
+    hold on
+    grid on
+    title("title")
 end
-return
-
-%% Visualize Data %%
-bar(10,2)
-hold on
-grid on
-bar(1)
-title("title")
