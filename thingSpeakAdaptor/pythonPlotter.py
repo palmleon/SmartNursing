cfd9cdbbf14636c44db62507198db5a6263f128e
alessda <99904423+alessda@users.noreply.github.com>
2022-04-13 21:19:59 +0200

    added some functions for data retrival and organisation still not 100% operative

--- thingSpeakAdaptor/pythonPlotter.py
+++ thingSpeakAdaptor/pythonPlotter.py
@@ -11,76 +11,86 @@ class pythonPlotter():
         self.saturation = []
         self.pulseRate = []
         self.perfusionIndex = []
-        self.label_map = ["battery oximeter", "perfusion index", "saturation",
-                          "pulse rate", "battery termometer", "temperature", "day_flag"]
+        self.label_map = ["battery oximeter", "perfusion index", "saturation","pulse rate", "battery termometer", "temperature", "day_flag"]
+        self.labelOfInterests = ['temperature','saturation','pulse rate']
 
-    def get(self, channelList):
-        # Read all entries made in the last 24 hours. Returns a JSON.
-        channelID = channelList[0]['id']
-        read_api = channelList[0]['api_keys'][1]['api_key']
-        r = requests.get(
-            'https://api.thingspeak.com/channels/{}/feeds.json?api_key={}'.format(channelID, read_api))
-        data = json.loads(r.text)
-        dic = dict()
-        for i in range(len(self.label_map)):
-            dic[self.label_map[i]] = []
-        for feed in data.get('feeds'):
-            for i in range(1, 8):
-                if feed.get('field'+str(i)) is not None:
-                    dic[self.label_map[i-1]].append(feed.get('field'+str(i)))
-        return dic
+    def retriveLatestData(self, channelList):
+        today = datetime.fromtimestamp(time.time()).day
+        dataList = []
+        for j in range(0, len(channelList)):
+            channelID = channelList[j]['id']
+            read_api = channelList[j]['api_keys'][1]['api_key']
+            r = requests.get(
+                'https://api.thingspeak.com/channels/{}/feeds.json?api_key={}'.format(channelID, read_api))
+            data = json.loads(r.text)
+            dic = dict()
+            dic['channelID'] = data['channel']['name']
+            for i in range(len(self.label_map)):
+                dic[self.label_map[i]] = []
+            for feed in data.get('feeds'):
+                dateReceived = feed.get('created_at')
+                date = dateReceived.split('-')[0] + '-' + dateReceived.split(
+                    '-')[1] + '-' + dateReceived.split('-')[2].split('T')[0]
+                for i in range(1, 8):
+                    if feed.get('field'+str(i)) is not None:
+                        newDict = dict()
+                        newDict[date] = feed.get('field'+str(i))
+                        dic[self.label_map[i-1]].append(newDict)
+            dataList.append(dic)
+        return dataList
 
-    def checkValues(self, dic):
-        for i in range(0, len(dic['temperature'])):
-            if float(dic['temperature'][i]) < 38:
-                dic['temperature'].remove(dic['temperature'][i])
-        for i in range(0, len(dic['perfusion index'])):
-            if float(dic['perfusion index'][i]) < 4:
-                dic['perfusion index'].remove(dic['perfusion index'][i])
-                dic['saturation'].remove(dic['perfusion index'][i])
-                dic['pulse rate'].remove(dic['perfusion index'][i])
-        return dic
-    def calculateAverage(self,dic,measure):
+    def getPatientsData(self,dic,patientID,measure,date):
+        chosenDataList = []
+        for i in range(0,len(dic)):
+            if dic[i]['channelID'] == patientID:
+                if len(dic[i][measure]) > 0:
+                    for j in range(0,len(dic[i][measure])):
+                        if date in dic[i][measure][j].keys():
+                            chosenDataList.append(dic[i][measure][j][date])
+        return chosenDataList
+
+    def getValuesFromKey(self,dic,key,measure):
+        for items in range(0,len(dic)):
+            if len(dic[items][measure]) > 0:
+                for i in range(0,len(dic[items][measure])):
+                    print(dic[items][measure][i])
+                    if key in dic[items][measure][i].keys():
+                        print(dic[items][measure][i][key])
+                        print(dic[items]['channelID'])
+
+    def calculateAverage(self,l):
         sum = 0
-        count = 0
-        for i in range(0,len(dic[measure])):
-            sum = sum + float(dic[measure][i])
-            count = count + 1
-        return sum/count
-    def dicToAvg(self,dic):
-        for keys in dic.keys():
-            if keys != 'battery oximeter' and keys != 'battery termometer' and keys!= 'day_flag':
-                if dic[keys]:
-                    dic[keys] = self.calculateAverage(dic,keys)
-        return dic
+        if l is not None:
+            if len(l) > 0:
+                for i in range(0,len(l)):
+                    if float(l[i]) > 35:
+                        sum  = sum + float(l[i])
+                avg = sum/len(l)
+                return avg
     
+    def dataOfInterest(self,dic):
+        pass
+
+    def saveToJson(self,dic):
+        json.dump(dic,open('test.json','w'))
     def plotter(self,dic):
         for keys in dic.keys():
-            if keys != 'battery oximeter' and keys != 'battery termometer' and keys!= 'day_flag':
-                if dic[keys]:
-                    pyplot.scatter(datetime.fromtimestamp(time.time()),dic[keys])
+            if dic[keys]:
+                pyplot.scatter(datetime.fromtimestamp(time.time()).day,dic[keys])
         pyplot.show()
-
-
-
+        self.saveToJson(dic)
 
 
 if __name__ == '__main__':
     plot = pythonPlotter()
     c = channelManager()
     c.listChannels()
-    v = plot.get(c.channelList)
-    v = plot.checkValues(v)
-    v = plot.dicToAvg(v)
-    plot.plotter(v)
-    # # x-axis values
-    # roll_num = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-    # # y-axis values
-    # marks = [55, 75, 96, 75, 36, 45, 87, 99, 100]
-
-
-    # pyplot.scatter(roll_num,datetime.fromtimestamp(time.time()))
-
-
-    # pyplot.show()
+    v = plot.retriveLatestData(c.channelList)
+    plot.saveToJson(v)
+    d = datetime.fromtimestamp(time.time())
+    d = d.replace(d.year,d.month,13)#d.day - 1)
+    d = str(d).split(' ')[0]
+    l = plot.getPatientsData(v,'5','temperature',d)
+    print(l)
+    print(plot.calculateAverage(l))
+    #plot.calculateAverage(v,'temperature')
