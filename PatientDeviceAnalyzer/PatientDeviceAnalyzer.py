282ab4050bf548fa9b98ac115bc657baf585ea30
Lorenzo Centamore <lorenzocentamore@MacBook-Pro.local>
2022-05-15 22:07:45 +0200

    prima bozza completa servizio

--- PatientDeviceAnalyzer/PatientDeviceAnalyzer.py
+++ PatientDeviceAnalyzer/PatientDeviceAnalyzer.py
@@ -15,9 +15,25 @@ class PatientDeviceAnalyzer():
     self.__name=conf_file["name"]
     self.__register=conf_file["host"]
     # Generazione template allert
-    self.__allert=conf_file["template_allarm"]
+    self.__alert=conf_file["template_alarm"]
     # Iscrizione al registro
     r = requests.post(self.__register+"/add-service",data= json.dumps({"serviceID" : self.__clientID, "name" : self.__name}))
+    
+    # Richiesta dati broker al registro
+    r = requests.get(self.__register+"/message-broker")
+    mb = r.json()
+    self.__broker=mb['name']
+    self.__port=mb['port']
+    # Richiesta del topic di publish
+    r = requests.get(self.__register+"/alarm-base-topic")
+    self.__base_topic_pub = r.json()
+
+    # Creating client
+    self.client = MyMQTT(self.__name, self.__broker, self.__port, None)
+    # Starting client
+    self.client.start()
+    time.sleep(2)
+
     # Richiesta table oraria
     r = requests.get(self.__register+"/patient-room-hourly-scheduling")
     d=r.json()
@@ -47,8 +63,20 @@ class PatientDeviceAnalyzer():
       # Confronto
       for device in self.__devices:
         if device not in actual:
+          ID_miss=device[:-1] #Prende l'Id del solo paziente
+          if device[-1]=="o":
+            sensor="saturimetro"
+          else:
+            sensor="termometro"
+          r=f"Il {sensor} del paziente {ID_miss} è stato scollegato"
           # Invio del messaggio di errore a telegram MQTT
-          print(f"Dispositivo {device} mancante")
+          print(r)
+          #Creazione messaggio
+          to_pub=self.__alert
+          to_pub["alert"]=r
+          to_pub["time"]=time.localtime()
+          # Publicazione alert
+          self.client.myPublish(self.__base_topic_pub+ID_miss,to_pub)
       # Aggiornamento lista dei dispositivi
       self.__devices=actual
     else: # E' giorno
