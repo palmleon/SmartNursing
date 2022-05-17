fc702ae85fbc8754bc74c561eaecfad9c6091926
alessda <99904423+alessda@users.noreply.github.com>
2022-05-17 18:36:17 +0200

    fixed dataAnalysis average + catalogue connection

--- DataAnalysis/dataAnalysis.py
+++ DataAnalysis/dataAnalysis.py
@@ -1,159 +1,148 @@
-from collections import defaultdict
 import json
 from channelManager import *
 import requests
+from datetime import date
+
 
 class dataAnalysis():
-    def __init__(self) -> None:
+    def __init__(self):
         self.conf_file = json.load(open('config.json'))
-        self.thingspeakMeasures = ["battery oximeter", "perfusion index", "saturation",
-                          "pulse rate", "battery termometer", "temperature", "day_flag"]
-        self.measureOfInterest = ["perfusion index", "saturation","pulse rate","temperature"]
-        r = requests.post(self.conf_file['host']+"/add-service", data=json.dumps({
-            'serviceID': 22,
-            'name': 'data-analysis'
+        r = requests.post(self.conf_file['host']+"/add-service",data =json.dumps( {
+            'serviceID' : 8,
+            'name' : 'data-analysis'
         }))
-        if r.ok == False:
-            print('Error adding the service')
-    def retriveLatestData(self, channelList):
-        """Retrive all data from thingspeak\n
-        Parameters
-        ----------
-        channelList: list
-            local list containing all thingspeak channels
-        Returns
-        -------
-        dataList: list
-            a list of dictionaries containing all latest data"""
-        dataList = []
-        for j in range(0, len(channelList)):
-            channelID = channelList[j]['id']
-            read_api = channelList[j]['api_keys'][1]['api_key']
+        r = requests.get(self.conf_file['host']+"/channel-data")
+        self.fields = r.json()
+        # self.fields = {'field1':'average saturation',
+        # 'field2': 'perfusion index', 
+        # 'field3': 'saturation', 
+        # 'field4':'pulse rate', 
+        # 'field5':'average pulse rate', 
+        # 'field6': 'temperature', 
+        # 'field7': 'average temperature', 
+        # 'field8': 'day_flag'}
+        r = requests.get(self.conf_file['host']+"/patient-room-hourly-scheduling")
+        r = r.json()
+        self.timeInterval = r['night']
+    def retriveData(self,channelList):
+        dataDict = dict()
+        for i in range(0,len(channelList)):
+            channelID = channelList[i]['id']
+            read_api = channelList[i]['api_keys'][1]['api_key']
             r = requests.get(
                 'https://api.thingspeak.com/channels/{}/feeds.json?api_key={}'.format(channelID, read_api))
-            data = json.loads(r.text)
-            print(data)
-            dic = dict()
-            dic['channelID'] = data['channel']['name']
-            for i in range(len(self.thingspeakMeasures)):
-                dic[self.thingspeakMeasures[i]] = []
-            for feed in data.get('feeds'):
-                dateReceived = feed.get('created_at')
-                date = dateReceived.split('-')[0] + '-' + dateReceived.split(
-                    '-')[1] + '-' + dateReceived.split('-')[2].split('T')[0]
-                for i in range(1, 8):
-                    if feed.get('field'+str(i)) is not None:
-                        newDict = dict()
-                        newDict[date] = feed.get('field'+str(i))
-                        dic[self.thingspeakMeasures[i-1]].append(newDict)
-            dataList.append(dic)
-        return dataList
-    def allDates(self,list):
-        l = []
-        for i in range(0,len(list)):
-            for m in self.measureOfInterest:
-                for j in range(0,len(list[i][m])):
-                    for k in list[i][m][j].keys():
-                        l.append(k)
-        l = self.removeDuplicates(l)
-        return l
-    def removeDuplicates(self,list):
-        newList = []
-        for i in range(0,len(list)):
-            if list[i] not in newList:
-                newList.append(list[i])
-        return newList
-    def temperatureAverage(self,list,patient,date):
-        temp = 0
+            data = r.json()
+            dataDict[data['channel']['name']] = data['feeds']
+        return dataDict
+    def uploadAVG(self,channelID,channelList,avgdata,stringField):
+        print('Upload in progress')
+        for i in range(0,len(channelList)):
+            if channelList[i]['name'] == channelID:
+                write_api = channelList[i]['api_keys'][0]['api_key']
+                read_api = channelList[i]['api_keys'][1]['api_key']
+                if stringField in self.fields.values():
+                    field_number = int(str(self.get_key(stringField,self.fields)).replace('field',''))
+                    requests.get('https://api.thingspeak.com/update?api_key={}&field{}={}'.format(
+                            write_api, field_number,avgdata))
+                    print('...')
+                    time.sleep(16)
+        print('Upload concluded')
+    def get_key(self,val,dic):
+        for key, value in dic.items():
+            if val == value:
+                return key
+    def convertData(self,stringDate):
+        date = dict()
+        date['year'] = stringDate[0:4]
+        date['month'] = stringDate[5:7]
+        date['day'] = stringDate[8:10]
+        date['hour'] = str(int(stringDate[11:13]) + 2)
+        date['minutes'] = stringDate[14:16]
+        date['seconds'] = stringDate[17:19]
+        return date
+    def checkDate(self,dicData):
+        today = date.today()
+        if dicData['year'] == str(today.year):
+            if dicData['month'] == str(today.month):
+                if dicData['day'] == str(today.day - 1):
+                    if dicData['hour'] >= '21':
+                        return True
+                elif dicData['day'] == str(today.day):
+                    if dicData['hour'] <= '10':
+                        return True
+        return False   
+    def averageTemp(self,temperatureList):
+        sum = 0
         count = 0
-        for i in range(0,len(list)):
-            if list[i]['channelID'] == patient:
-                for j in range(0,len(list[i]['temperature'])):
-                    if date in list[i]['temperature'][j].keys():
-                        if float(list[i]['temperature'][j][date]) >= 35:
-                            temp = temp + float(list[i]['temperature'][j][date])
-                            count = count + 1
-        if count > 0:
-            return temp/count
-        return 0
-    def piAverage(self,list,patient,date):
-        sat = 0
-        pulse = 0
+        for i in range(0,len(temperatureList)):
+            if float(temperatureList[i]) >= 35:
+                sum = sum + float(temperatureList[i])
+                count = count + 1
+                print(temperatureList[i],sum,count)
+        if count != 0:
+            print(sum/count)
+            return sum/count
+        else:
+            return 0
+    def averagePi(self,pi,saturation,pulse):
+        sumSat = 0
         countSat = 0
         countPulse = 0
-        for i in range(0,len(list)):
-            if list[i]['channelID'] == patient:
-                leng = len(list[i]['perfusion index'])
-                if leng > len(list[i]['saturation']) or leng > len(list[i]['pulse rate']):
-                    if len(list[i]['saturation']) > len(list[i]['pulse rate']):
-                        leng = len(list[i]['pulse rate'])
-                    else:
-                        leng = len(list[i]['saturation'])
-                for j in range(0,leng):
-                    if date in list[i]['perfusion index'][j].keys():
-                        if float(list[i]['perfusion index'][j][date]) > 4:
-                            if date in list[i]['saturation'][j].keys():
-                                sat = sat + float(list[i]['saturation'][j][date])
-                                countSat = countSat + 1
-                            if date in list[i]['pulse rate'][j].keys():
-                                pulse = pulse + float(list[i]['pulse rate'][j][date])
-                                countPulse = countPulse + 1
-        if countPulse > 0:
-            avgPulse = pulse/countPulse
+        sumPulse = 0
+        if len(pi) >= len(saturation) or len(pi) >= len(pulse):
+            if len(saturation) >= len(pulse):
+                l = len(pulse)
+            else:
+                l = len(saturation)
         else:
-            avgPulse = 0
-        if countSat > 0:
-            avgSat = sat/countSat
+            l = len(pi)
+        for i in range(0,l):
+            if float(pi[i]) >= 4:
+                sumSat = sumSat + float(saturation[i])
+                countSat = countSat + 1
+                sumPulse = sumPulse + float(pulse[i])
+                countPulse = countPulse + 1
+        if countSat !=0:
+            avgSat = sumSat/countSat
         else:
             avgSat = 0
+        if countPulse !=0:
+            avgPulse = sumPulse/countPulse
+        else:
+            avgPulse = 0
         return avgPulse,avgSat
-    def calcAverage(self,list,date):
-        d = defaultdict(dict)
-        for i in range(0,len(list)):
-            for j in range(0,len(date)):
-                d[list[i]['channelID']][date[j]] = self.temperatureAverage(list,list[i]['channelID'],date[j])
-        avgTemp = d
-        d = defaultdict(dict)
-        e = defaultdict(dict)
-        for i in range(0,len(list)):
-            for j in range(0,len(date)):
-                d[list[i]['channelID']][date[j]],e[list[i]['channelID']][date[j]] = self.piAverage(list,list[i]['channelID'],date[j])
-        avgPulse = d
-        avgSat = e
-        return avgTemp,avgPulse,avgSat
-    def saveToJson(self, dic):
-        json.dump(dic, open('averageData.json', 'w'))
-    def retriveID(self,list):
-        ids = []
-        for i in range(0,len(list)):
-            ids.append(list[i]['id'])
-        return ids
-
-def performID():
-    plot = dataAnalysis()
-    c = channelManager()
-    c.listChannels()
-    ids = plot.retriveID(c.channelList)
-    return ids
-
 def perform():
-    plot = dataAnalysis()
-    c = channelManager()
+    conf_file = json.load(open('config.json'))
+    c = channelManager(conf_file['host'])
+    d = dataAnalysis()
     c.listChannels()
-    latestData = plot.retriveLatestData(c.channelList)
-    latestData = plot.removeBatteryData(latestData)
-    listDate = plot.allDates(latestData)
-    avgTemp,avgPulse,avgSat = plot.calcAverage(latestData,listDate)
-    d = dict()
-    d['temperature'] = avgTemp
-    d['pulse rate'] = avgPulse
-    d['saturation'] = avgSat
-    print(avgTemp)
-    plot.saveToJson(d)
+    latestDate = d.retriveData(c.channelList)
+    for keys in latestDate.keys():
+        tempList = []
+        piList = []
+        satList = []
+        pulseList = []
+        for i in range(0,len(latestDate[keys])):
+            check = d.checkDate(d.convertData(\
+                latestDate[keys][i]['created_at']))
+            check = True ########## TEST ###########
+            if check == True:
+                if latestDate[keys][i]['field6'] != None:
+                    tempList.append(latestDate[keys][i]['field6'])
+                if latestDate[keys][i]['field2'] != None:
+                    piList.append(latestDate[keys][i]['field2'])
+                if latestDate[keys][i]['field3'] != None:
+                    satList.append(latestDate[keys][i]['field3'])
+                if latestDate[keys][i]['field4'] != None:
+                    pulseList.append(latestDate[keys][i]['field4'])
+        temperature = d.averageTemp(tempList)
+        pulse,saturation = d.averagePi(piList,satList,pulseList)
+        d.uploadAVG(keys,c.channelList,temperature,'average temperature')
+        d.uploadAVG(keys,c.channelList,pulse,'average pulse rate')
+        d.uploadAVG(keys,c.channelList,saturation,'average saturation')
+
 
 if __name__ == '__main__':
     perform()
 
-
-
-
+    
