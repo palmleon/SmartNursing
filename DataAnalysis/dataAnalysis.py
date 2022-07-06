dd0787e94cb2725a0e5e1c76beb8abca595b04ba
Alessandro D'Apice <alessandrodapice@Alessandros-MacBook-Pro.local>
2022-07-06 12:13:20 +0200

    bugfixes, UTC+2 fixed

--- DataAnalysis/dataAnalysis.py
+++ DataAnalysis/dataAnalysis.py
@@ -17,6 +17,8 @@ class dataAnalysis():
         r = requests.get(self.conf_file['host']+"/patient-room-hourly-scheduling")
         r = r.json()
         self.timeInterval = r['night']
+        self.timeInterval = [18,22] ######### TEST #######
+        self.watingTime = 60*2
     def retriveData(self,channelList):
         dataDict = dict()
         for i in range(0,len(channelList)):
@@ -45,24 +47,64 @@ class dataAnalysis():
         for key, value in dic.items():
             if val == value:
                 return key
+    def strUTC2(self,date):
+        bhour = date['hour']
+        bday = date['day']
+        bmonth = date['month']
+        if bhour == '00':
+            date['hoour'] == '02'
+            date['day'] == str(int(date['day']) + 1)
+            if bmonth == '11' or bmonth == '06' or bmonth =='04' or bmonth =='09':
+                if bday == '30':
+                    date['day'] == '01'
+                    date['month'] == str(int(date['month']) + 1)
+            elif bmonth == '02':
+                if bday == '28':
+                    date['day'] == '01'
+                    date['month'] == str(int(date['month']) + 1)
+            elif bmonth == '01' or bmonth == '03' or bmonth == '05' or \
+                bmonth == '07' or bmonth == '08' or bmonth == '10' or \
+                bmonth == '12':
+                if bday == '31':
+                    date['day'] == '01'
+                    date['month'] == str(int(date['month']) + 1)
+            if bmonth == '12' and bday == '31':
+                date['year'] == str(int(date['year']) + 1)
+        else:
+            date['hour'] = str(int(date['hour']) + 2)
+        return date
     def convertData(self,stringDate):
         date = dict()
         date['year'] = stringDate[0:4]
         date['month'] = stringDate[5:7]
         date['day'] = stringDate[8:10]
-        date['hour'] = str(int(stringDate[11:13]) + 2)
+        date['hour'] = str(int(stringDate[11:13]))
         date['minutes'] = stringDate[14:16]
         date['seconds'] = stringDate[17:19]
+        date = self.strUTC2(date)
         return date
     def checkDate(self,dicData):
         today = date.today()
-        if dicData['year'] == str(today.year):
-            if dicData['month'] == str(today.month):
-                if dicData['day'] == str(today.day - 1):
-                    if dicData['hour'] >= self.timeInterval[0]:
+        print(dicData)
+        if int(dicData['year']) == today.year:
+            print('year')
+            if int(dicData['month']) == today.month:
+                print('month')
+                if int(dicData['day']) == today.day - 1:
+                    print('day1')
+                    if int(dicData['hour']) >= self.timeInterval[0]:
+                        print('hour1')
                         return True
-                elif dicData['day'] == str(today.day):
-                    if dicData['hour'] <= self.timeInterval[1]:
+                elif int(dicData['day']) == today.day:
+                    print('day2')
+                    if int(dicData['hour']) <= self.timeInterval[1]:
+                        print('hour2')
+                        return True
+            elif int(dicData['month']) == today.month - 1:
+                if int(dicData['hour']) >= self.timeInterval[0]:
+                    return True
+                elif int(dicData['day']) == today.day:
+                    if int(dicData['hour']) <= self.timeInterval[1]:
                         return True
         return False   
     def averageTemp(self,temperatureList):
@@ -119,22 +161,31 @@ def perform():
     c = channelManager(conf_file['host'])
     d = dataAnalysis()
     flag_time = 1
-    lastTimeExec = 0
+    print('Starting data analysis')
+    lastTimeExec = datetime.fromtimestamp(time.time())
+    lastTimeExec = d.convertData(str(lastTimeExec))
     while True:
         now = datetime.fromtimestamp(time.time())
-        print('LastTimeExec: ' + str(lastTimeExec))
-        print('Now: ' + str(now))
+        now = d.convertData(str(now))
+        print('LastTimeExec: ' + lastTimeExec['hour']+':'+lastTimeExec['minutes']+':'+\
+            lastTimeExec['seconds']+'  '+lastTimeExec['day']+'/'+lastTimeExec['month']+'/'+\
+                lastTimeExec['year'])
+        print('Now: ' + now['hour']+':'+now['minutes']+':'+\
+            now['seconds']+'  '+now['day']+'/'+now['month']+'/'+\
+                now['year'])
         if lastTimeExec != 0:
-            if now.day > lastTimeExec.day:
-                if now.hour >= d.timeInterval[1]:
-                    if now.hour <= d.timeInterval[0]:
+            if int(now['day']) > int(lastTimeExec['day']):
+                if int(now['hour']) >= d.timeInterval[1]:
+                    if int(now['hour']) <= d.timeInterval[0]:
                         flag_time = 1
                         lastTimeExec = now
+                        print('A day has passed, time to check data\n')
             else:
                 flag_time = 0
-        if lastTimeExec == 0:
-            lastTimeExec = now
-            flag_time = 1
+        count = 0 ########### TEST ########
+        if count == 0: ########### TEST ########
+            flag_time = 1 ########### TEST ########
+            count = 1 ########### TEST ########
         if flag_time == 1:
             c.listChannels()
             if len(c.channelList) > 0:
@@ -144,11 +195,15 @@ def perform():
                     piList = []
                     satList = []
                     pulseList = []
+                    temperature = None
+                    pulse = None
+                    saturation = None
                     print('Checking patient ' + str(keys) + ' data')
                     for i in range(0,len(latestDate[keys])):
                         check = d.checkDate(d.convertData(\
                             latestDate[keys][i]['created_at']))
                         #check = True ########## TEST ###########
+                        print(check)
                         if check == True:
                             if latestDate[keys][i]['field5'] != None:
                                 tempList.append(latestDate[keys][i]['field5'])
@@ -158,8 +213,12 @@ def perform():
                                 satList.append(latestDate[keys][i]['field1'])
                             if latestDate[keys][i]['field3'] != None:
                                 pulseList.append(latestDate[keys][i]['field3'])
-                    temperature = d.averageTemp(tempList)
-                    pulse,saturation = d.averagePi(piList,satList,pulseList)
+                    if len(tempList) >= 1:
+                        temperature = d.averageTemp(tempList)
+                    if len(piList) >= 1:
+                        if len(satList) >= 1:
+                            if len(pulseList) >= 1:
+                                pulse,saturation = d.averagePi(piList,satList,pulseList)
                     if temperature != None:
                         d.uploadAVG(keys,c.channelList,temperature,'average temperature')
                     if pulse != None:
@@ -167,7 +226,8 @@ def perform():
                     if saturation != None:
                         d.uploadAVG(keys,c.channelList,saturation,'average saturation')
                     print('\n')
-        time.sleep(60*2)
+        print('Repeating check after ' + str(d.watingTime) + ' minutes')
+        time.sleep(d.watingTime)
         
 if __name__ == '__main__':
     perform()
