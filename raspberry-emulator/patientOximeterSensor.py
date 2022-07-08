6a18c3d0e7fcc5f58cf8e34af6f703f942bf202c
Lorenzo Centamore <lorenzocentamore@MacBook-Pro.station>
2022-07-08 16:57:22 +0200

    Modifica casistica, aggiunta casi

--- raspberry-emulator/patientOximeterSensor.py
+++ raspberry-emulator/patientOximeterSensor.py
@@ -3,7 +3,7 @@ import time
 
 class Oximeter_sensor():
     def __init__(self):
-        self.__baseMessage={"bn" :"testTemeperature","bt":0,"e":[{"n":"battery","u":"V","v":0},{"n":"perfusion index","u":"perc","v":0},{"n":"saturation","u":"perc","v":0}, {"n":"pulse rate","u":"bpm","v":0}]}
+        self.__baseMessage={"bn" :"testOximeter","bt":0,"e":[{"n":"battery","u":"V","v":0},{"n":"perfusion index","u":"perc","v":[]},{"n":"saturation","u":"perc","v":[]}, {"n":"pulse rate","u":"bpm","v":[]}]}
     
     def emulate(self,ID_P):
         # Una  lista di 10 valori ogni 10 secondi di PI, saturazione e battiti (associati ad un solo valore di batteria)
@@ -55,12 +55,18 @@ class Oximeter_sensor():
             self.__baseMessage["e"][1]["v"]= (random.uniform(low=4,high=20,size= 10).tolist())
             self.__baseMessage["e"][2]["v"]= (random.randint(low=96,high=100,size= 10).tolist())
             self.__baseMessage["e"][3]["v"]= (random.randint(low=70,high=90,size= 10).tolist())
-        else:
+        elif ID_P==9:
             # Paziente combinato saturazione bassa e battito alto
             self.__baseMessage["e"][0]["v"]=random.uniform(low=2.81,high=3.0)
             self.__baseMessage["e"][1]["v"]= (random.uniform(low=4,high=20,size= 10).tolist())
             self.__baseMessage["e"][2]["v"]= (random.randint(low=93,high=95,size= 10).tolist())
             self.__baseMessage["e"][3]["v"]= (random.randint(low=100,high=104,size= 10).tolist())
+        else:
+            # Paziente combinato saturazione bassa e battito basso
+            self.__baseMessage["e"][0]["v"]=random.uniform(low=2.81,high=3.0)
+            self.__baseMessage["e"][1]["v"]= (random.uniform(low=4,high=20,size= 10).tolist())
+            self.__baseMessage["e"][2]["v"]= (random.randint(low=93,high=95,size= 10).tolist())
+            self.__baseMessage["e"][3]["v"]= (random.randint(low=50,high=55,size= 10).tolist())
 
         #self.__baseMessage["bn"]=ID_P
         self.__baseMessage["bt"]=time.time()
--- raspberry-emulator/patientTemperatureSensor.py
+++ raspberry-emulator/patientTemperatureSensor.py
@@ -3,7 +3,7 @@ import time
 
 class Temperature_sensor():
     def __init__(self):
-        self.__baseMessage={"bn" : "","bt":0,"e" : [{"n":"battery","u":"V","v":0}, {"n":"temperature","u":"cel","v":0}]}
+        self.__baseMessage={"bn" : "testTemperature","bt":0,"e" : [{"n":"battery","u":"V","v":0}, {"n":"temperature","u":"cel","v":0}]}
     
     def emulate(self,ID_P):
         # 1 dato di batteria e temperatura insieme ogni 30 secondi
@@ -19,10 +19,14 @@ class Temperature_sensor():
             # Paziente con termometro spostato e con batteria carica (non faccio entrambi i casi, in quanto l'analizzatore si ferma prima mandando solo batteria scarica)
             self.__baseMessage["e"][0]["v"]=random.uniform(low=2.81,high=3.0)
             self.__baseMessage["e"][1]["v"]=random.uniform(low=34,high=35)
-        else:
+        elif ID_P==4:
             # Paziente con febbre e con batteria carica
             self.__baseMessage["e"][0]["v"]=random.uniform(low=2.81,high=3.0)
             self.__baseMessage["e"][1]["v"]=random.uniform(low=37.1,high=37.5)
+        else:
+            # Paziente con temperatura bassa e con batteria carica
+            self.__baseMessage["e"][0]["v"]=random.uniform(low=2.81,high=3.0)
+            self.__baseMessage["e"][1]["v"]=random.uniform(low=35.1,high=35.9)
         
         #self.__baseMessage["bn"]=ID_P
         self.__baseMessage["bt"]=time.time()
