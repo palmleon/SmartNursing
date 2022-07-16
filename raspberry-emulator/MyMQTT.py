e1e9457eb8aac0d629be7a927e9c532a79613b3d
Lorenzo Centamore <lorenzocentamore@MacBook-Pro.local>
2022-07-16 18:54:03 +0200

    Adding new version of MyMQTT in all services

--- raspberry-emulator/MyMQTT.py
+++ raspberry-emulator/MyMQTT.py
@@ -2,13 +2,11 @@ import paho.mqtt.client as PahoMQTT
 import json
 class MyMQTT:
     def __init__(self, clientID, broker, port, notifier):
-        #topics è nella forma di lista di tuple (topic,qos)
         self.broker = broker
         self.port = port
         self.notifier = notifier
         self.clientID = clientID
-        self._topic = []
-        self._isSubscriber = False
+        self._topic = [] #lista of tuples (topic,qos)
         # create an instance of paho.mqtt.client
         self._paho_mqtt = PahoMQTT.Client(clientID,True)  
         # register the callback
@@ -20,7 +18,7 @@ class MyMQTT:
         print ("Connected to %s with result code: %d" % (self.broker, rc))
         if self._topic!=[]:
             self._paho_mqtt.subscribe(self._topic)
-            print(f"\nsubscribing to all topics: {self._topic}\n")
+            print(f"Subscribing to all topics: {self._topic}")
 
 
 
@@ -32,22 +30,16 @@ class MyMQTT:
     def myPublish (self, topic, msg):
         # publish a message with a certain topic
         self._paho_mqtt.publish(topic, json.dumps(msg), 2)
-        print("\nPublished:")
+        print("Published:")
         print(msg)
-        print(f"At Topic: {topic}\n")
+        print(f"At Topic: {topic}")
 
  
-    def mySubscribe (self, topic=None):
-        if topic!=None:
-            # In questo caso, ho dato in input un topic, quindi si sottoscrive
-            # con qos fisso pari a 2
-            
-            # subscribe for a topic
-            self._paho_mqtt.subscribe(topic, 2) 
-            # just to remember that it works also as a subscriber
-            self._isSubscriber = True
-            self._topic.append((topic,2)) # Concatena ai topic a cui è sottoscritto (per tenerne traccia)
-            print ("\nsubscribed to %s\n" % (topic))
+    def mySubscribe (self, topic):
+        # subscribe for a topic
+        self._paho_mqtt.subscribe(topic, 2)
+        self._topic.append((topic,2)) # Concatena ai topic a cui è sottoscritto (per tenerne traccia)
+        print ("subscribed to %s" % (topic))
  
     def start(self):
         #manage connection to broker
@@ -55,16 +47,11 @@ class MyMQTT:
         self._paho_mqtt.loop_start()
         
     def unsubscribe(self):
-        if (self._isSubscriber):
-            #ATTENZIONE attualmente così toglie la sottoscrizione a tutti i Topic
-            # remember to unsuscribe if it is working also as subscriber 
-            self._paho_mqtt.unsubscribe(self._topic)
+        for topic in self._topic: 
+            self._paho_mqtt.unsubscribe(topic[0])
+        self._topic=[]
             
     def stop (self):
-        if (self._isSubscriber):
-            # remember to unsuscribe if it is working also as subscriber 
-            self._paho_mqtt.unsubscribe(self._topic)
-            self._topic=[]
- 
+        self.unsubscribe()
         self._paho_mqtt.loop_stop()
         self._paho_mqtt.disconnect()
